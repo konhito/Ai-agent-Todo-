3f01cd1d813e6d70f35be87425b1599c672f8ad7
konhito <konhito02@gmail.com>
2025-01-26 12:31:09 +0530

    generated drizzle

--- db/schema.js
+++ db/schema.js
@@ -1,9 +1,8 @@
-import { timeStamp } from "console";
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const todosTable = pgTable("todos", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   todo: text().notNull(),
-  createdAt: timeStamp("createdAt").defaultNow(),
-  updatedAt: timeStamp("createdAt").$onUpdate(() => new Data()),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
 });
